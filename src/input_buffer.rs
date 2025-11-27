c3b370a706038adb248b64873321991d7555730d
Kumar Ujjawal <ujjawalpathak6@gmail.com>
2025-11-27 22:31:28 +0530

    simple repl

---
+++ src/input_buffer.rs
@@ -0,0 +1,29 @@
+use std::io::{self, Write};
+
+pub struct InputBuffer {
+    pub buffer: String,
+}
+
+impl InputBuffer {
+    pub fn new() -> Self {
+        Self {
+            buffer: String::new(),
+        }
+    }
+
+    pub fn print_prompt() {
+        print!("db > ");
+        io::stdout().flush().unwrap();
+    }
+
+    pub fn read_input(&mut self) -> io::Result<()> {
+        self.buffer.clear();
+        io::stdin().read_line(&mut self.buffer)?;
+
+        self.buffer = self.buffer.trim().to_string();
+
+        Ok(())
+    }
+
+    pub fn close(self) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod input_buffer;
+use crate::input_buffer::InputBuffer;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_buffer = InputBuffer::new();
+
+    loop {
+        InputBuffer::print_prompt();
+
+        if let Err(err) = input_buffer.read_input() {
+            eprintln!("Error reading input: {}", err);
+            continue;
+        }
+
+        if input_buffer.buffer.is_empty() {
+            continue;
+        }
+
+        match input_buffer.buffer.as_str() {
+            "exit()" => {
+                input_buffer.close();
+                println!("Bye!");
+                break;
+            }
+
+            cmd => {
+                println!("Unrecognized command: {}", cmd);
+            }
+        }
+    }
 }
