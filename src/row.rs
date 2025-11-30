c0b1d7bed221b1a858211a0dcf83a6319f72f872
Kumar Ujjawal <ujjawalpathak6@gmail.com>
2025-11-30 21:12:59 +0530

    from values for row with thiserror

--- src/row.rs
+++ src/row.rs
@@ -8,6 +8,14 @@ pub struct Row {
     pub email: [u8; COLUMN_EMAIL_SIZE],
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum RowError {
+    #[error("username is too long")]
+    UserNameTooLong,
+    #[error("email is too long")]
+    EmailTooLong,
+}
+
 impl Row {
     pub fn empty() -> Self {
         Self {
@@ -15,5 +23,47 @@ impl Row {
             username: [0; COLUMN_USERNAME_SIZE],
             email: [0; COLUMN_EMAIL_SIZE],
         }
+    }
+
+    /// Create a Row with validation
+    pub fn from_values(id: u32, username: &str, email: &str) -> Result<Self, RowError> {
+        let username_bytes = username.as_bytes();
+        let email_bytes = email.as_bytes();
+
+        if username_bytes.len() > COLUMN_USERNAME_SIZE {
+            return Err(RowError::UserNameTooLong);
+        }
+
+        if email_bytes.len() > COLUMN_EMAIL_SIZE {
+            return Err(RowError::EmailTooLong);
+        }
+
+        let mut row = Row::empty();
+        row.id = id;
+
+        row.username[..username_bytes.len()].copy_from_slice(username_bytes);
+        row.email[..email_bytes.len()].copy_from_slice(email_bytes);
+
+        Ok(row)
+    }
+
+    /// Interpret username as str
+    ///
+    pub fn username_as_str(&self) -> &str {
+        let end = self
+            .username
+            .iter()
+            .position(|&b| b == 0)
+            .unwrap_or(COLUMN_USERNAME_SIZE);
+        std::str::from_utf8(&self.username[..end]).unwrap_or("")
+    }
+
+    pub fn email_as_str(&self) -> &str {
+        let end = self
+            .email
+            .iter()
+            .position(|&b| b == 0)
+            .unwrap_or(COLUMN_EMAIL_SIZE);
+        std::str::from_utf8(&self.email[..end]).unwrap_or("")
     }
 }
